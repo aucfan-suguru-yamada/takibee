7eeebb0ccf9c1a9db0aa3ef70289b8d85d4f5ee4
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-04-27 18:38:05 +0900

    add: キャンプの画像登録

---
+++ app/javascript/packs/table_link.js
@@ -0,0 +1,8 @@
+//jQuery.js（もしくは対象HTMLの<script>タグ内に記述）
+$(document).on('turbolinks:load',function() {
+  //clickable-rowクラスの行をマウスオーバーするとカーソルをポインターに変更
+  $(".clickable-row").css("cursor","pointer").click(function() {
+  //クリックで「data-href」のリンク先へ遷移
+    window.location = $(this).data("link");
+  });
+});
