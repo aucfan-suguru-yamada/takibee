1c4d5091e07468c658c89fca3a0470252f3cced9
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-05-13 23:07:51 +0900

    add:サイドバーの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,12 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
-require("./preview")
-require("./table_link")
-require("./camp_image_preview")
+require("channels");
+require("./preview");
+require("./table_link");
+require("./camp_image_preview");
+require("admin-lte");
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
