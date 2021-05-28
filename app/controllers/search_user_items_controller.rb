c6e6f081b32df61a4330ea00c7cf013727cf8aaa
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-05-28 19:01:12 +0900

    fix: アイテム追加画面の見た目

--- app/controllers/search_user_items_controller.rb
+++ app/controllers/search_user_items_controller.rb
@@ -1,7 +1,8 @@
 class SearchUserItemsController < ApplicationController
 
   def index
-    if params[:keyword]
+
+    if params[:keyword].present?
       # @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], tagId: '1001908')
       @items = RakutenWebService::Ichiba::Product.search(keyword: params[:keyword])
       render 'index'
