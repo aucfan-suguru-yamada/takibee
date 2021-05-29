f1417abb5b1defcb7d573c054b771b6d057ae263
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-05-29 21:30:02 +0900

    fix: every_campのN+1問題

--- app/controllers/every_camp_controller.rb
+++ app/controllers/every_camp_controller.rb
@@ -1,6 +1,6 @@
 class EveryCampController < ApplicationController
   def index
-    @camps = Camp.all
+    @camps = Camp.all.includes(user: { avatar_attachment: :blob }, items: { small_image_attachment: :blob }).with_attached_images
     @like = Like.new
   end
 end
