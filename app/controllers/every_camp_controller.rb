fae942cc07baedb9e37bf5dd03b3612383840574
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-06-02 21:27:16 +0900

    add: 投稿の読み込み機能

--- app/controllers/every_camp_controller.rb
+++ app/controllers/every_camp_controller.rb
@@ -1,9 +1,10 @@
 class EveryCampController < ApplicationController
   def index
-    @camps = Camp.all.includes(:area,
+    @camps = Camp.includes(:area,
                               user: { avatar_attachment: :blob },
                               items: { small_image_attachment: :blob },
                               ).with_attached_images.order("camped_on DESC")
+    @camps = @camps.page(params[:page]).without_count
     @like = Like.new
   end
 end
