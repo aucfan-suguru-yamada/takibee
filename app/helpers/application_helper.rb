4ffade4eeb472698461051ff2d5ff748d6d4bfa8
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-06-17 07:47:34 +0900

    add: gem-meta-tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,35 @@ module ApplicationHelper
   def before_login_active_if(path, action)
     path == controller_path && action == action_name ? 'active-nav-item' : ''
   end
+
+  # meta-tagの設定
+  def default_meta_tags
+    {
+      site: 'TAKiBEE.',
+      title: 'TAKiBEE.',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'キャンプを記録するサービスです。キャンプ地・アイテム・写真の記録を残そう。',
+      keywords: 'TAKiBEE,キャンプ,キャンプギア,キャンプアイテム',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('apple-touch-icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      ],
+      og: {
+        site_name: :site, # もしくは site_name: :site
+        title: :title, # もしくは title: :title
+        description: :description, # もしくは description: :description
+        type: 'website',
+        url: request.original_url,
+        image: image_url('takibee_icon.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary',
+        site: '@Soogle_1729',
+      }
+    }
+  end
 end
