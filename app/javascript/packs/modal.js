4d833db3a831287d014c8458f247152096fe61e7
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-06-27 23:42:08 +0900

    fix: 投稿画像一覧に画像3枚表示

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,11 +1,17 @@
 //モーダル使えないかなあ
-$(document).on("click", '.js-modal-img', function(){
-  var src = $(this).children().children().attr('src');
+$(document).on("click", 'img', function(){
+  //var src = $(this).children().children().attr('src');
+  //var src = $(this).children().children().attr('src');
+  var img_id = $(this).attr('id');
+  var modal_img_id = 'modal_' + img_id;
+  console.log(img_id);
+  console.log(modal_img_id);
+  var src = $(`#${modal_img_id}`).attr('src')
   $('.bigimg').children().attr('src', src);
+
 
   var modal = document.getElementById( 'modal01' );
   $( modal ).fadeIn( 300 );
-  console.log(modal);
   return false;
 });
 
