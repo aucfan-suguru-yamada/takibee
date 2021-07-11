8dc4eb5b1578cff8b2c5484d8244272cf62de2ff
Yamada Suguru <yamadasuguru@MacBook-Pro.local>
2021-07-11 15:34:57 +0900

    rspec: ログイン

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "user-#{n}" }
+    sequence(:email) {|n| "test-#{n}@example.com"}
     password { 'password' }
     password_confirmation { 'password' }
+  end
+
+  factory :guest_user, class: User do
+    name {'guest_user'}
+    email {'guest_user@example.com'}
+    password {'password'}
+    password_confirmation {'password'}
   end
 end
--- spec/support/system_helper.rb
+++ spec/support/system_helper.rb
@@ -1,24 +1,17 @@
 module SystemHelper
-  def login_as_general
+  def login
     general_user = create(:user)
     visit '/login'
     fill_in 'メールアドレス', with: general_user.email
-    fill_in 'パスワード', with: '12345678'
+    fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
   end
 
-  def login_as_admin
-    admin_user = create(:user, :admin)
-    visit admin_login_path
-    fill_in 'メールアドレス', with: admin_user.email
-    fill_in 'パスワード', with: '12345678'
-    click_button 'ログイン'
-  end
-
-  def login_as_user(user)
+  def login_as_guest_user
+    guest_user = create(:user)
     visit login_path
     fill_in 'メールアドレス', with: user.email
-    fill_in 'パスワード', with: '12345678'
+    fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
   end
 end
